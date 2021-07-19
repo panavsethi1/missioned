1227f6218bcf015053b63ac87ad5cd3a936d7c9a
PanavSethi <panav.sethi.delhi@gmail.com>
2021-07-20 02:54:51 +0530

    chore: sass folder structure setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "./App.css";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <h1>I hope my App is working (:)</h1>
+    </div>
+  );
 }
 
 export default App;
